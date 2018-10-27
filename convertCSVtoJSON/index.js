7909e5216a466531c643185cedcb33fcb25d78eb
Justin Lee <justin.lee@ubc.ca>
2018-10-27 11:33:38 -0700

    Added converted json files

--- convertCSVtoJSON/index.js
+++ convertCSVtoJSON/index.js
@@ -1,6 +1,9 @@
 const csv = require('csvtojson')
+const { writeFileSync } = require('fs')
 const { join } = require('path')
 
+const fileName = `2011Parking_Tickets.csv`
+
 csv()
-  .fromFile(join(__dirname, './source/2018Parking_Tickets.csv'))
-  .then(json => console.log(json))
+  .fromFile(join(__dirname, `./source/${fileName}`))
+  .then(json => writeFileSync(join(__dirname, `./output/${fileName}.json`), JSON.stringify(json), 'utf8'))
