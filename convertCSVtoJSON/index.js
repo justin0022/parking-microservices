49516f22b88a81d1efd73fc1b591f95af773a7db
Justin Lee <justin.lee@ubc.ca>
2018-10-27 12:27:15 -0700

    Working on populating DB

--- convertCSVtoJSON/index.js
+++ convertCSVtoJSON/index.js
@@ -1,9 +1,16 @@
 const csv = require('csvtojson')
 const { writeFileSync } = require('fs')
 const { join } = require('path')
+require('dotenv').config()
+const mongoClient = require('mongodb').MongoClient
 
-const fileName = `2011Parking_Tickets.csv`
+// const fileName = `2011Parking_Tickets.csv`
 
-csv()
-  .fromFile(join(__dirname, `./source/${fileName}`))
-  .then(json => writeFileSync(join(__dirname, `./output/${fileName}.json`), JSON.stringify(json), 'utf8'))
+// csv()
+//   .fromFile(join(__dirname, `./source/${fileName}`))
+//   .then(json => writeFileSync(join(__dirname, `./output/${fileName}.json`), JSON.stringify(json), 'utf8'))
+const url = encodeURIComponent(process.env.PASSWORD)
+
+mongoClient.connect(`mongodb://covparkingticket:${url}@covparkingticket.documents.azure.com:10255/?ssl=true`, (err, db) => {
+  db.
+})
